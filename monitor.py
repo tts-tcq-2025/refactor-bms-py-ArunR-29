cf1a4b62e4716bf88c0baaf0437fc1a7019a8447
ArunR-29 <arunvicky1124@gmail.com>
2025-08-22 09:25:54 +0530

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,37 +1,19 @@
-
 from time import sleep
 import sys
 
-
 def vitals_ok(temperature, pulseRate, spo2):
-  if temperature > 102 or temperature < 95:
-    print('Temperature critical!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  elif pulseRate < 60 or pulseRate > 100:
-    print('Pulse Rate is out of range!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  elif spo2 < 90:
-    print('Oxygen Saturation out of range!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  return True
+    def alert(msg):
+        print(msg)
+        for _ in range(6):
+            for symbol in ['* ', ' *']:
+                print('\r' + symbol, end='')
+                sys.stdout.flush()
+                sleep(1)
+        return False
+
+    if temperature > 102 or temperature < 95:
+        return alert('Temperature critical!')
+    if pulseRate < 60 or pulseRate > 100:
+        return alert('Pulse Rate is out of range!')
+    if spo2 < 90:
+        return alert('Oxygen Saturation out of
