cddaa79e14f1821d2bee7aeb187935334057f868
ArunR-29 <arunvicky1124@gmail.com>
2025-08-22 09:35:55 +0530

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,19 +1,24 @@
-from time import sleep
-import sys
+def vitals_status(temperature, pulseRate, spo2):
+    if temperature > 102 or temperature < 95:
+        return False, 'Temperature critical!'
+    if pulseRate < 60 or pulseRate > 100:
+        return False, 'Pulse Rate is out of range!'
+    if spo2 < 90:
+        return False, 'Oxygen Saturation out of range!'
+    return True, 'All vitals are normal'
+
+def show_alert(msg):
+    from time import sleep
+    import sys
+    print(msg)
+    for _ in range(6):
+        for symbol in ['* ', ' *']:
+            print('\r' + symbol, end='')
+            sys.stdout.flush()
+            sleep(1)
 
 def vitals_ok(temperature, pulseRate, spo2):
-    def alert(msg):
-        print(msg)
-        for _ in range(6):
-            for symbol in ['* ', ' *']:
-                print('\r' + symbol, end='')
-                sys.stdout.flush()
-                sleep(1)
-        return False
-
-    if temperature > 102 or temperature < 95:
-        return alert('Temperature critical!')
-    if pulseRate < 60 or pulseRate > 100:
-        return alert('Pulse Rate is out of range!')
-    if spo2 < 90:
-        return alert('Oxygen Saturation out of
+    status, msg = vitals_status(temperature, pulseRate, spo2)
+    if not status:
+        show_alert(msg)
+    return
