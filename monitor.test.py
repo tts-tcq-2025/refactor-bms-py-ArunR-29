8f214c66c513b75785d60221d533f8c13dc5f891
ArunR-29 <arunvicky1124@gmail.com>
2025-08-22 10:09:26 +0530

    Update monitor.test.py

--- monitor.test.py
+++ monitor.test.py
@@ -3,10 +3,27 @@ from monitor import vitals_ok
 
 
 class MonitorTest(unittest.TestCase):
-    def test_not_ok_when_any_vital_out_of_range(self):
-        self.assertFalse(vitals_ok(99, 102, 70))
-        self.assertTrue(vitals_ok(98.1, 70, 98))
+    def test_temperature_out_of_range(self):
+        status, msg = vitals_ok(103, 80, 95)
+        self.assertFalse(status)
+        self.assertEqual(msg, 'Temperature critical!')
+
+    def test_pulse_out_of_range(self):
+        status, msg = vitals_ok(98, 120, 95)
+        self.assertFalse(status)
+        self.assertEqual(msg, 'Pulse Rate is out of range!')
+
+    def test_spo2_out_of_range(self):
+        status, msg = vitals_ok(98, 80, 85)
+        self.assertFalse(status)
+        self.assertEqual(msg, 'Oxygen Saturation out of range!')
+
+    def test_all_vitals_normal(self):
+        status, msg = vitals_ok(98.6, 75, 98)
+        self.assertTrue(status)
+        self.assertEqual(msg, 'All vitals are normal')
 
 
 if __name__ == '__main__':
   unittest.main()
+
